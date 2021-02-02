786fc3ce2fc6702827441b2c26f6c1f0fb19625a
Katharine <kwchuiaa@connect.ust.hk>
2021-02-02 17:43:40 +0100

    change architecture probing

--- update.sh
+++ update.sh
@@ -2,30 +2,30 @@
 #set -e
 
 repo="https://f-droid.org/repo/"
+MAIN_ARCH="x86_64"
+SUB_ARCH="x86"
 
 addCopy() {
 	addition=""
 	if [ "$native" != "" ]
 	then
 		unzip bin/$1 "lib/*"
-		if [ "$native" == "arm64-v8a" ];then
+		if [ "$native" == "$MAIN_ARCH" ];then
 			addition="
+LOCAL_MODULE_TARGET_ARCH := $MAIN_ARCH
 LOCAL_PREBUILT_JNI_LIBS := \\
-$(unzip -lv bin/$1 |grep -v Stored |sed -nE 's;.*(lib/arm64-v8a/.*);\t\1 \\;p')
-
+$(unzip -lv bin/$1 |grep -v Stored |sed -nE 's;.*(lib/'"$MAIN_ARCH"'/.*);\t\1 \\;p')
 			"
 		fi
-		if [ "$native" == "armeabi-v7a" ];then
+		if [ "$native" == "$SUB_ARCH" ];then
 			addition="
+LOCAL_MODULE_TARGET_ARCH := $SUB_ARCH
 LOCAL_PREBUILT_JNI_LIBS := \\
-$(unzip -lv bin/$1 |grep -v Stored |sed -nE 's;.*(lib/armeabi-v7a/.*);\t\1 \\;p')
-
-LOCAL_MULTILIB := 32
-
+$(unzip -lv bin/$1 |grep -v Stored |sed -nE 's;.*(lib/'"$SUB_ARCH"'/.*);\t\1 \\;p')
 			"
 		fi
 	fi
-    if [ "$2" == com.google.android.gms ] || [ "$2" == com.android.vending ] ;then
+    if [ "$2" == com.google.android.gms ] || [ "$2" == com.android.vending ] || [ "$2" == org.fdroid.fdroid.privileged ] || [ "$2" == com.farmerbb.taskbar ];then
         addition="LOCAL_PRIVILEGED_MODULE := true"
     fi
 cat >> Android.mk <<EOF
@@ -65,41 +65,74 @@ downloadFromFdroid() {
 	#marketvercode="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]' -v ./marketvercode tmp/index.xml || true)"
 	#apk="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package[versioncode="'"$marketvercode"'"]' -v ./apkname tmp/index.xml || xmlstarlet sel -t -m '//application[id="'"$1"'"]/package[1]' -v ./apkname tmp/index.xml)"
 	index=1
-	while true
-	do
-		echo $index
-		apk="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./apkname tmp/index.xml)"
-		native="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./nativecode tmp/index.xml)"
-		echo $apk
-		echo $native
-		if [ "$native" != "" ] && [ "$(echo $native | grep arm64-v8a)" != "" ]
+	apk="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./apkname tmp/index.xml)"
+	native="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./nativecode tmp/index.xml)"
+	if [ "$native" != "" ]
+	then
+		index=1
+		while true
+		do
+			apk="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./apkname tmp/index.xml)"
+			native="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./nativecode tmp/index.xml)"
+			if [ "$native" != "" ] && [ "$(echo $native | grep $MAIN_ARCH)" != "" ]
+			then
+				native=$MAIN_ARCH
+				break
+			fi
+			if [ "$native" == "" ]
+			then
+				break
+			fi
+			index=$((index + 1))
+		done
+		if [ "$native" != "$MAIN_ARCH" ]
 		then
-			native="arm64-v8a"
-			break
-		elif [ "$native" != "" ] && [ "$(echo $native | grep armeabi-v7a)" != "" ]
-		then
-			native="armeabi-v7a"
-			break
-		elif [ "$native" = "" ]
-		then
-			break
+			index=1
+			while true
+			do
+				apk="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./apkname tmp/index.xml)"
+				native="$(xmlstarlet sel -t -m '//application[id="'"$1"'"]/package['$index']' -v ./nativecode tmp/index.xml)"
+				if [ "$native" != "" ] && [ "$(echo $native | grep $SUB_ARCH)" != "" ]
+				then
+					native=$SUB_ARCH
+					break
+				fi
+				index=$((index + 1))
+			done
+			if [ "$native" != "$SUB_ARCH" ]
+			then
+				echo $1 is not available in $MAIN_ARCH nor $SUB_ARCH
+				exit 1
+			fi
 		fi
-		index=$((index + 1))
-	done
+	fi
     if [ ! -f bin/$apk ];then
         while ! wget --connect-timeout=10 $repo/$apk -O bin/$apk;do sleep 1;done
     fi
 	addCopy $apk $1 "$2"
-
 }
 
 
 #fdroid
 downloadFromFdroid org.fdroid.fdroid
+#fdroid extension
+downloadFromFdroid org.fdroid.fdroid.privileged
 #vlc
 downloadFromFdroid org.videolan.vlc
+#terminal
+downloadFromFdroid jackpal.androidterm
+#task bar
+#downloadFromFdroid com.farmerbb.taskbar
 #icecat
-downloadFromFdroid org.gnu.icecat "Browser2 QuickSearchBox"
+#downloadFromFdroid org.gnu.icecat "Browser2"
+#downloadFromFdroid org.gnu.icecat "Browser2 QuickSearchBox"
+#lawnchair
+#downloadFromFdroid ch.deletescape.lawnchair.plah
+#bliss launcher
+#downloadFromFdroid foundation.e.blisslauncher
+#downloadFromFdroid foundation.e.blisslauncher "Launcher3QuickStep"
+#shelter
+downloadFromFdroid net.typeblog.shelter
 #phh's Superuser
 #downloadFromFdroid me.phh.superuser
 #Ciphered SMS
@@ -148,6 +181,26 @@ downloadFromFdroid com.fsck.k9 "Email"
 #YouTube viewer
 #downloadFromFdroid org.schabi.newpipe
 
+repo=https://fdroid.bromite.org/fdroid/repo
+downloadFromFdroid org.bromite.bromite "Browser2"
+
+#open weather provider
+#wget https://mirrorbits.lineageos.org/WeatherProviders/20190718/OpenWeatherProvider-16.0-signed.apk -O bin/OpenWeatherProvider.apk
+#cat >> Android.mk <<EOF
+#include \$(CLEAR_VARS)
+#LOCAL_MODULE := OpenWeatherProvider
+#LOCAL_MODULE_TAGS := optional
+#LOCAL_SRC_FILES := bin/OpenWeatherProvider.apk
+#LOCAL_MODULE_CLASS := APPS
+#LOCAL_CERTIFICATE := PRESIGNED
+#include \$(BUILD_PREBUILT)
+
+#EOF
+
+#echo -e "\tOpenWeatherProvider \\" >> apps.mk
+
 echo >> apps.mk
+
+
 
 rm -Rf tmp
